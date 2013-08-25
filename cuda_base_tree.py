f681b34961741b1e6c7a512d0154b4d95020acad
yisheng <yisheng@beaker-20.news.cs.nyu.edu>
2013-08-25 06:55:21 +0000

    Further refactoring, seperate node class and base class to seperate files.

---
+++ cuda_base_tree.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class BaseTree(object):
+  def __init__(self):
+    self.root = None
+    self.max_depth = None
+
+  def print_tree(self):
+    def recursive_print(node):
+      if node.left_child and node.right_child:
+        print node
+        recursive_print(node.left_child)
+        recursive_print(node.right_child)
+      else:
+        print node
+    assert self.root is not None
+    recursive_print(self.root)
+
+  def __predict(self, val):
+    temp = self.root
+    while True:
+      if temp.left_child and temp.right_child:
+        if val[temp.feature_index] < temp.feature_threshold:
+          temp = temp.left_child
+        else:
+          temp = temp.right_child
+      else: 
+          return temp.value
+
+  def predict(self, inputs):
+    res = []
+    for val in inputs:
+      res.append(self.__predict(val))
+    return np.array(res)
+
+
---
+++ node.py
@@ -0,0 +1,18 @@
+
+class Node(object):
+  def __init__(self):
+    self.value = None 
+    self.error = None
+    self.samples = None
+    self.feature_threshold = None
+    self.feature_index = None
+    self.left_child = None
+    self.right_child = None
+    self.height = None
+
+  def __str__(self):
+    if self.left_child and self.right_child:
+      return "[NODE] Height: %s, FeatureIndex: %s, Threshold: %d, Samples: %d" % (self.height, self.feature_index, self.feature_threshold, self.samples)
+    else:
+      return "[LEAF] Height: %s, Samples: %s" % (self.height, self.samples)
+
