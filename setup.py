4526a47822c38715c6523865608835cd23355f26
yisheng <yisheng@geeker-2.news.cs.nyu.edu>
2013-09-10 06:19:34 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from setuptools import setup, find_packages
 import cuda_tree
 
@@ -6,6 +7,7 @@ setup(
     version = cuda_tree.__version__,
     description = "building decison tree on Cuda",
     author = ["Alex Rubinsteyn", "Yisheng Liao"],
+    author_email = ["alexr@cs.nyu.edu", "yl1912@nyu.edu"],
     packages = find_packages() + ['cuda_tree.test'],
     package_dir = {'cuda_tree.test' : './test'},
     package_data = {'cuda_tree' : ['cuda_kernels/*.cu']},
@@ -13,6 +15,12 @@ setup(
         'numpy',
         'sklearn',
         'pycuda'
-      ])
+      ],
+    classifiers=['Development Status :: 1 - Alpha',
+                  'Topic :: Software Development :: Libraries',
+                  'License :: OSI Approved :: BSD License',
+                  'Intended Audience :: Developers',
+                  'Programming Language :: Python :: 2.7',
+                ])
 
 
