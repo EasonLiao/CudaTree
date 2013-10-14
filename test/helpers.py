796cdfd2d1c670812966dcbd583c0c739a513166
EasonLiao <liaoyisheng89@gmail.com>
2013-10-14 22:51:51 +0000

    change small mistakes in helper.py

--- test/helpers.py
+++ test/helpers.py
@@ -9,7 +9,7 @@ def compare_accuracy(x,y, n_estimators = 11, bootstrap = True, slop = 0.98, n_re
   ytest = y[n:]
   cudarf = RandomForestClassifier(n_estimators = n_estimators, bootstrap = bootstrap)
   import sklearn.ensemble
-  skrf = sklearn.ensemble.RandomForestClassifier(n_estimators = n_estimators, bootstrap = bootstrap)
+  skrf = sklearn.ensemble.RandomForestClassifier(n_estimators = n_estimators, bootstrap = bootstrap, max_features = "log2")
   cuda_score_total = 0 
   sk_score_total = 0
   for i in xrange(n_repeat):
@@ -24,4 +24,4 @@ def compare_accuracy(x,y, n_estimators = 11, bootstrap = True, slop = 0.98, n_re
     cuda_score_total += cuda_score 
 
   assert cuda_score_total >= (sk_score_total * slop), \
-    "Getting significantly worse test accuracy than sklearn: %s vs. %s" % (cuda_score_total / n_repeat, sk_score / n_repeat)
+    "Getting significantly worse test accuracy than sklearn: %s vs. %s" % (cuda_score_total / n_repeat, sk_score_total / n_repeat)
