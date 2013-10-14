237cbdafc444ef347cde1c81138294fc663a207d
EasonLiao <liaoyisheng89@gmail.com>
2013-10-14 20:10:46 +0000

    change corresponding test file

--- test/test_cuda_iris.py
+++ test/test_cuda_iris.py
@@ -7,8 +7,8 @@ x_test, y_test = load_data("iris")
 
 def test_iris():
   with timer("Cuda treelearn"):
-    forest = RandomForestClassifier()
-    forest.fit(x_train, y_train, n_trees=10, max_features = 4, bootstrap = False)
+    forest = RandomForestClassifier(bootstrap = False)
+    forest.fit(x_train, y_train)
   with timer("Predict"):
     diff, total = util.test_diff(forest.predict(x_test), y_test)  
     print "%s(Wrong)/%s(Total). The error rate is %f." % (diff, total, diff/float(total))
