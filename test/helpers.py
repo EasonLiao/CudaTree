f76a00255f53396798de188098b3f05c170b2d12
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-10-14 18:30:26 -0400

    repeat loop in accuracy test

--- test/helpers.py
+++ test/helpers.py
@@ -1,20 +1,27 @@
 
 from cudatree import RandomForestClassifier
 
-def compare_accuracy(x,y, n_estimators = 21, bootstrap = True, slop = 0.98):
+def compare_accuracy(x,y, n_estimators = 11, bootstrap = True, slop = 0.98, n_repeat = 10):
   n = x.shape[0] / 2 
   xtrain = x[:n]
   ytrain = y[:n]
   xtest = x[n:]
   ytest = y[n:]
-  n_estimators = 10
   cudarf = RandomForestClassifier(n_estimators = n_estimators, bootstrap = bootstrap)
-  cudarf.fit(xtrain, ytrain)
   import sklearn.ensemble
   skrf = sklearn.ensemble.RandomForestClassifier(n_estimators = n_estimators, bootstrap = bootstrap)
-  skrf.fit(xtrain, ytrain)
-  sk_score = skrf.score(xtest, ytest)
-  cuda_score = cudarf.score(xtest, ytest)
-  print "Sklearn score", sk_score 
-  print "CudaTree score", cuda_score 
-  assert cuda_score >= (sk_score * slop), "Getting significantly worse test accuracy than sklearn: %s vs. %s" % (cuda_score, sk_score)
+  cuda_score_total = 0 
+  sk_score_total = 0
+  for i in xrange(n_repeat):
+    cudarf.fit(xtrain, ytrain)
+    skrf.fit(xtrain, ytrain)
+    sk_score = skrf.score(xtest, ytest)
+    cuda_score = cudarf.score(xtest, ytest)
+    print "Iteration", i 
+    print "Sklearn score", sk_score 
+    print "CudaTree score", cuda_score 
+    sk_score_total += sk_score 
+    cuda_score_total += cuda_score 
+
+  assert cuda_score_total >= (sk_score_total * slop), \
+    "Getting significantly worse test accuracy than sklearn: %s vs. %s" % (cuda_score_total / n_repeat, sk_score / n_repeat)
