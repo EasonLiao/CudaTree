9f4b048d74ead67a94ce1cc7ec4de5ea1fa04625
EasonLiao <liaoyisheng89@gmail.com>
2013-11-14 21:34:52 +0000

    test hybrid benchmark and change the awkward spelling mistake

--- benchmark/benchmark_all.py
+++ benchmark/benchmark_all.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python
 from cudatree import RandomForestClassifier, load_data, timer
 from cudatree import util
+from hybridforest import RandomForestClassifier as hybridForest
 
 bfs_threshold = 1000
 debug = False
 verbose = False
 bootstrap = False
 
-def benchmark(dataset):
+def benchmark_cuda(dataset):
   x_train, y_train = load_data(dataset)
   #Just use this forest to compile the code.
   throw_away = RandomForestClassifier(n_estimators = 1, bootstrap = bootstrap, verbose = False, 
         max_features = None, debug = debug)
   throw_away.fit(x_train, y_train, bfs_threshold = bfs_threshold)
 
-  with timer("%s benchmark" % (dataset,)): 
+  with timer("%s benchmark cuda" % (dataset,)): 
     forest = RandomForestClassifier(n_estimators = 50, bootstrap = bootstrap, verbose = verbose, 
         max_features = None, debug = debug)
     forest.fit(x_train, y_train, bfs_threshold = bfs_threshold)
 
-benchmark("cf100")
-benchmark("kdd")
-benchmark("covtype")
-benchmark("cf10")
-benchmark("inet")
+def benchmark_hybrid(dataset):
+  x_train, y_train = load_data(dataset)
+  #Just use this forest to compile the code.
+  throw_away = hybridForest(n_estimators = 1, bootstrap = bootstrap,  
+        max_features = None)
+  throw_away.fit(x_train, y_train, bfs_threshold = bfs_threshold)
 
+  with timer("%s benchmark hybrid" % (dataset,)): 
+    forest = hybridForest(n_estimators = 50, bootstrap = bootstrap, 
+        max_features = None)
+    forest.fit(x_train, y_train, bfs_threshold = bfs_threshold)
+
+
+benchmark_hybrid("cf100")
+benchmark_hybrid("kdd")
+benchmark_hybrid("covtype")
+benchmark_hybrid("cf10")
+
+benchmark_cuda("cf100")
+benchmark_cuda("kdd")
+benchmark_cuda("covtype")
+benchmark_cuda("cf10")
 
--- hybridforest/hybridforest.py
+++ hybridforest/hybridforest.py
@@ -2,35 +2,28 @@
 import sklearn
 from sklearn.ensemble import RandomForestClassifier as skRF
 import numpy as np
-import time
 from cudatree import load_data, RandomDecisionTreeSmall, timer, convert_result
 from cudatree import RandomForestClassifier as cdRF
 import multiprocessing
-from multiprocessing import Value, Lock
-import itertools
-from PyWiseRF import WiseRF
+from multiprocessing import Value, Lock, cpu_count
 
-def sklearn_build(X, Y, n_estimators, bootstrap, max_features, remain_trees, result_queue, lock):
-  n_trees = 5
+
+def sklearn_build(X, Y, n_estimators, bootstrap, max_features, n_jobs, remain_trees, result_queue, lock):
   forests = list()
-
   if max_features == None:
     max_features = "auto"
   
   while True:
     lock.acquire()
-    if remain_trees.value == 0:
+    if remain_trees.value < 2 * n_jobs:
       lock.release()
       break
 
-    if remain_trees.value < n_trees:
-      n_trees = remain_trees.value
-
-    remain_trees.value -= n_trees
+    remain_trees.value -= n_jobs
     lock.release()
 
-    print "sklearn : ", n_trees 
-    f = skRF(n_estimators = n_trees, n_jobs = n_trees, bootstrap = bootstrap, max_features = max_features)
+    print "sklearn : ", n_jobs
+    f = skRF(n_estimators = n_jobs, n_jobs = n_jobs, bootstrap = bootstrap, max_features = max_features)
     f.fit(X, Y)
     forests.append(f)
 
@@ -39,12 +32,15 @@ def sklearn_build(X, Y, n_estimators, bootstrap, max_features, remain_trees, res
 
 
 class RandomForestClassifier(object):
-  def __init__(self, n_estimators = 10, max_features = None, bootstrap = True):
+  def __init__(self, n_estimators = 10, n_jobs = -1, max_features = None, bootstrap = True):
     self.n_estimators = n_estimators
     self.max_features = max_features
     self.bootstrap = bootstrap
     self._sk_forests = None
     self._cuda_forest = None
+    if n_jobs == -1:
+      n_jobs = cpu_count()
+    self.n_jobs = n_jobs
 
 
   def _cuda_fit(self, X, Y, bfs_threshold, remain_trees, lock):
@@ -72,7 +68,6 @@ class RandomForestClassifier(object):
           f.n_labels, f.COMPT_THREADS_PER_BLOCK, f.RESHUFFLE_THREADS_PER_BLOCK, 
           f.max_features, f.min_samples_split, bfs_threshold, f.debug, f)   
       
-      print remain_trees.value
       si, n_samples = f._get_sorted_indices(f.sorted_indices)
       tree.fit(f.samples, f.target, si, n_samples)
       f.forest.append(tree) 
@@ -92,16 +87,16 @@ class RandomForestClassifier(object):
 
     #Start a new process to do sklearn random forest
     p = multiprocessing.Process(target = sklearn_build, args = (X, Y, self.n_estimators, 
-      self.bootstrap, self.max_features, remain_trees, result_queue, lock))
-    
+      self.bootstrap, self.max_features, self.n_jobs - 1, remain_trees, result_queue, lock))
+      
     p.daemon = False
     p.start() 
 
     #At same time, we construct cuda radom forest
-    self._cuda_fit(X, Y, bfs_threshold, remain_trees, lock)
-    
+    self._cuda_fit(X, Y, bfs_threshold, remain_trees, lock)    
     #get the result
     self._sk_forests = result_queue.get()
+    p.join()
 
 
   def predict(self, X):
@@ -113,10 +108,7 @@ class RandomForestClassifier(object):
      
     n_sk_trees = self.n_estimators - len(self._cuda_forest.forest)
     n_cd_trees = self.n_estimators - n_sk_trees
-    
-
     cuda_proba = self._cuda_forest.predict_proba(X) * n_cd_trees
-
     final_proba = (sk_proba  + cuda_proba ) / self.n_estimators
     res = np.array([np.argmax(final_proba[i]) for i in xrange(final_proba.shape[0])])
     
@@ -124,15 +116,15 @@ class RandomForestClassifier(object):
       res = convert_result(self._cuda_forest.compt_table, res)
     return res
 
+
   def score(self, X, Y):
     return np.mean(self.predict(X) == Y)
 
 
 if __name__ == "__main__":
-  x_train, y_train = load_data("inet")
+  x_train, y_train = load_data("cf100")
   x_test = x_train[0:100]
   y_test = y_train[0:100]
-  x_test, y_test = load_data("inet_test")
 
   with timer("hybird version"):
     f = RandomForestClassifier(50, max_features = None, bootstrap = False)
